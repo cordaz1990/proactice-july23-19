3ef36f8b147ea82b6618a90013bd6bbf104b2274
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 20:59:06 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,21 @@
-words = ['d', 'r', 't', 'y']
-print ('h','e','l','l','o')
+#string slicing
+
+>>> fruit = 'banana'
+>>> fruit[:3]
+'ban'
+>>> fruit[3:]
+'ana'
+
+greeting = 'Hello, World!'
+new_greeting = 'J' + greeting[1:]
+new_greeting
+'Jello, world!"
+
+def find(word, letter)
+    index = 0 
+    while index < len(word):
+      if word[index] == letter:
+          return index
+      index = index + 1
+    return -1
+      
